8425b46325308de18142d2873c77699e73231584
Taeyoung Son <329tyson@gmail.com>
2018-12-01 12:24:33 +0900

    moved mmap_file to process.c / aded process_file to process.c

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,6 +2,19 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+struct process_file {
+    int fd;
+    struct file *file;
+    struct list_elem elem;
+};
+
+struct mmap_file// table of file mappings에 사용됨
+{
+	struct hash_elem mmap_elem;//hash element로 사용
+	struct list page_list;//해당 파일이 저장된 page들의 list
+	int mapid;//mmap을 통해 할당받은 mapid
+	struct file *file;//mapping한 file을 저장
+};
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
